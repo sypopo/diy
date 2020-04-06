69c70c9b4193371ae2b80a55090bf0082f3061fb
sypopo <sypopo@qq.com>
2020-04-07 00:19:56 +0800

    DIY

---
+++ diy-x86-1907.sh
@@ -0,0 +1,36 @@
+#/bin/bash
+#=================================================
+#   Description: DIY script
+#   Lisence: MIT
+#   Author: P3TERX
+#   Blog: https://p3terx.com
+#=================================================
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+#echo '修改feeds'
+#sed -i '1,2s/coolsnowwolf/hong0980/g' ./feeds.conf.default
+
+echo '修改网关地址'
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+echo '修改时区'
+sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
+
+echo '修改机器名称'
+sed -i 's/OpenWrt/OpenWrt-x64/g' package/base-files/files/bin/config_generate
+
+echo '修改banner'
+cp -f diy/sypopo/banner package/base-files/files/etc/
+
+echo '添加软件包'
+cp -f diy/sypopo/zzz-default-settings package/default-settings/files/
+git clone https://github.com/vernesong/OpenClash.git && mv OpenClash/luci-app-openclash package/luci-app-openclash
+git clone https://github.com/sypopo/luci-app-ssr-plus.git package/sypopo/
+mkdir -p package/luci-app-diskman && \
+wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Makefile -O package/luci-app-diskman/Makefile
+mkdir -p package/parted && \
+wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Parted.Makefile -O package/parted/Makefile
+
+echo '当前路径'
+pwd
+
