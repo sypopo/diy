7a1abe671680ee2bf1d96203043be90e4e3c4415
sypopo <sypopo@qq.com>
2021-01-24 00:14:54 +0800

    Update diy-x86-Lean.sh

--- diy-x86-Lean.sh
+++ diy-x86-Lean.sh
@@ -24,8 +24,8 @@ date=`date +%m.%d.%Y`
 sed -i "s/SyPopo$/SyPopo $date/g" package/base-files/files/etc/banner
 
 echo '添加软件包'
-git clone https://github.com/vernesong/OpenClash.git package/vernesong/luci-app-openclash
-git clone https://github.com/xiaorouji/openwrt-passwall.git package/xiaorouji/openwrt-passwall
+git clone https://github.com/vernesong/OpenClash.git package/lean/luci-app-openclash
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/lean/openwrt-passwall
 git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
 git clone https://github.com/fw876/helloworld.git package/lean/luci-app-ssr-plus
 git clone https://github.com/Leo-Jo-My/luci-theme-opentomato.git package/lean/luci-theme-opentomato
