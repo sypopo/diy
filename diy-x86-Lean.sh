f507d5b91e5d5054d24e278f085bab642dcaae57
sypopo <sypopo@qq.com>
2021-01-24 00:13:56 +0800

    Update diy-x86-Lean.sh

--- diy-x86-Lean.sh
+++ diy-x86-Lean.sh
@@ -25,6 +25,7 @@ sed -i "s/SyPopo$/SyPopo $date/g" package/base-files/files/etc/banner
 
 echo '添加软件包'
 git clone https://github.com/vernesong/OpenClash.git package/vernesong/luci-app-openclash
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/xiaorouji/openwrt-passwall
 git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
 git clone https://github.com/fw876/helloworld.git package/lean/luci-app-ssr-plus
 git clone https://github.com/Leo-Jo-My/luci-theme-opentomato.git package/lean/luci-theme-opentomato
