2390c4d07eb0c0f0b455f05472e1a17b342855c8
sypopo <sypopo@qq.com>
2021-02-03 09:47:50 +0800

    Update diy-x86-Lienol.sh

--- diy-x86-Lienol.sh
+++ diy-x86-Lienol.sh
@@ -25,9 +25,12 @@ sed -i "s/SyPopo$/SyPopo $date/g" package/base-files/files/etc/banner
 
 echo '添加软件包'
 #sed -i 's/0.32.1/0.33.0/g' package/diy/frp/Makefile
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
 git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
 git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 git clone https://github.com/fw876/helloworld.git package/helloworld
+rm -rf feeds/packages/libs/libcap
+svn co https://github.com/openwrt/packages/trunk/libs/libcap feeds/packages/libs/libcap
 
 echo '定义默认值'
 cat > package/default-settings/files/zzz-default-settings <<-EOF
