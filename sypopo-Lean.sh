5836729c24a1aea028be3ef2dcd0d6cb10e3c8bc
sypopo <sypopo@qq.com>
2022-09-11 09:03:12 +0800

    Update sypopo-Lean.sh

--- sypopo-Lean.sh
+++ sypopo-Lean.sh
@@ -10,7 +10,7 @@
 #sed -i '1,2s/coolsnowwolf/sypopo/g' ./feeds.conf.default
 
 echo '修改网关地址'
-sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 echo '修改时区'
 sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
